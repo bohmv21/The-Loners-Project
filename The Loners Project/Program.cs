56673830db38fa92ee396d3f50ed58ddcee6eaac
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-10-09 12:53:08 +0200

    .

--- The Loners Project/Program.cs
+++ The Loners Project/Program.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
-namespace The_Loners_Project
+namespace Escape_room
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string IntroText = "Welcome to the escape room";
+            var varIntroText = "";
+            for (int i = 0; i < IntroText.Length; i++)
+            {
+                varIntroText += IntroText[i];
+                Console.Clear();
+                Console.WriteLine(varIntroText);
+                Thread.Sleep(50);
+            }
+            Thread.Sleep(1000);
 
-            Console.WriteLine("bo & juul is een fucking twat");
-            Console.ReadLine();
-            Console.ReadLine();
 
-            // test
+            string IntroRules = "There are some keywords u will need to use for this escape room :";
+            var varIntroRules = "";
+            for (int i = 0; i < IntroRules.Length; i++)
+            {
+                varIntroRules += IntroRules[i];
+                Console.Clear();
+                Console.WriteLine(varIntroRules);
+                Thread.Sleep(50);
+            }
+
+            string IntroKeyWords = "Inspect";
+            var varIntroKeyWords = "";
+            for (int i = 0; i < IntroKeyWords.Length; i++)
+            {
+                varIntroKeyWords += IntroKeyWords[i];
+                Console.Clear();
+                Console.WriteLine(IntroRules);
+                Console.WriteLine(varIntroKeyWords);
+                Thread.Sleep(50);
+            }
+            Console.ReadLine();
+            Console.WriteLine("");
         }
     }
 }
